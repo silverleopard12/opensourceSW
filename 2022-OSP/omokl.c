be1bf71c89a5e6205f76d402fdb374b7722f5726
silverleopard12 <42231481+silverleopard12@users.noreply.github.com>
2022-04-27 06:07:04 +0900

    Add files via upload
    opensource end

---
+++ 2022-OSP/omokl.c
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
+#define Third_Run "Starbridge.wav"
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define SPACE 32
+#define ESC 27
+#define U1 1
+#define U2 2
+
+int MAP_X, MAP_Y;
+int** GMap;
+
+typedef struct XY {
+	int x;
+	int y;
+}xy;
+
+gotoxy(int x, int y)
+{
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void initGame()
+{
+	int i = 0, k = 0;
+	for (i = 0; i < MAP_Y; i++)
+	{
+		for (k = 0; k < MAP_X; k++)
+		{
+			if (i == 0)
+			{
+				if (k == 0)
+					printf("¦£");
+				else if (k + 1 == MAP_X)
+					printf("¦¤");
+				else
+					printf("¦¨");
+			}
+			else if (i + 1 < MAP_Y)
+			{
+				if (k == 0)
+					printf("¦§");
+				else if (k + 1 == MAP_X)
+					printf("¦©");
+				else
+					printf("¦«");
+			}
+			else
+			{
+				if (k == 0)
+					printf("¦¦");
+				else if (k + 1 == MAP_X)
+					printf("¦¥");
+				else
+					printf("¦ª");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int searchStone(xy hd, int **GMap, int flag, int p, int sw)
+{
+	if (GMap[hd.y][hd.x] != flag) return 0;
+	if (p == 0) // À§ ¾Æ·¡ Å½»ö
+	{
+		hd.y += sw;
+	}
+
+	else if (p == 1) // ÁÂ ¿ì Å½»ö
+	{
+		hd.x += sw;
+	}
+
+	else if (p == 2) // ¿ÞÂÊ À§ -> ¿À¸¥ÂÊ ¾Æ·¡
+	{
+		hd.x += sw;
+		hd.y += sw;
+	}
+
+	else // ¿À¸¥ÂÊ À§ -> ¿ÞÂÊ ¾Æ·¡
+	{
+		hd.x += sw;
+		hd.y -= sw;
+	}
+	return 1 + searchStone(hd, GMap, flag, p, sw);
+}
+
+void checkStone(xy hd, int **GMap, int turn)
+{
+	int i = 0, countStone = 0;
+	for (i = 0; i < 4; i++)
+	{
+		countStone = 0;
+		countStone += searchStone(hd, GMap, turn, i, 1);
+		countStone += searchStone(hd, GMap, turn, i, -1);
+		if (countStone == 6)
+		{
+			gotoxy(0, MAP_Y);
+			if (turn == U1)
+				printf("»ç¿ëÀÚ 1");
+			else
+				printf("»ç¿ëÀÚ 2");
+			printf("´ÔÀÌ ½Â¸®ÇÏ¼Ì½À´Ï´Ù.!");
+			getch();
+			exit(1);
+		}
+	}
+}
+
+void startGame(int **GMap)
+{
+	char ip = '\0';
+	int turn = U1;
+	xy hd = { MAP_X / 2, MAP_Y / 2 };
+
+	while (1)
+	{
+		if (kbhit()){
+			ip = getch();
+			switch (ip)
+			{
+
+			case LEFT:
+				if (hd.x > 0)
+					hd.x -= 1;
+				break;
+
+			case RIGHT:
+				if (hd.x < MAP_X - 1)
+					hd.x += 1;
+				break;
+
+			case UP:
+				if (hd.y > 0)
+					hd.y -= 1;
+				break;
+
+			case DOWN:
+				if (hd.y < MAP_Y - 1)
+					hd.y += 1;
+				break;
+
+			case SPACE:
+				if (GMap[hd.y][hd.x] == 0)
+				{
+					gotoxy(hd.x * 2, hd.y);
+					if (turn == U1)
+					{
+						GMap[hd.y][hd.x] = U1;
+						printf("¡Ü");
+						checkStone(hd, GMap, turn);
+						turn = U2;
+					}
+					else
+
+					{
+						GMap[hd.y][hd.x] = U2;
+						printf("¡Û");
+						checkStone(hd, GMap, turn);
+						turn = U1;
+					}
+				}
+				break;
+
+			case ESC:
+				exit(1);
+				break;
+			}
+
+			gotoxy(hd.x * 2, hd.y);
+		}
+	}
+}
+
+int main() {
+	PlaySound(TEXT(Third_Run), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	system("mode con cols=222 lines=70");
+	printf("STARBRIDGE\n");
+	printf("> KEY - Arrow keys, Spaces bar\n");
+	while (1) {
+		printf("> Enter the height(MAX = 63, MIN = 32)\n");
+		scanf_s("%d", &MAP_Y);
+		if (MAP_Y < 32 || MAP_Y > 63) {
+			printf("Invalid value. Please re-enter\n");
+		}
+		else {
+			break;
+		}
+	}
+	while (1) {
+		printf("> Enter the width(MAX = 200, MIN = 100) \n");
+		scanf_s("%d", &MAP_X);
+		if (MAP_X < 100 || MAP_X > 200) {
+			printf("Invalid value. Please re-enter\n");
+		}
+		else {
+			break;
+		}
+	}
+
+	GMap = (int**)malloc(sizeof(int*) * MAP_Y);
+	GMap[0] = (int*)malloc(sizeof(int) * MAP_Y, MAP_X);
+	for (int i = 1; i < MAP_Y; i++) {
+		GMap[i] = GMap[i - 1] + MAP_X;
+	}
+
+	printf("\nheight: %d, width: %d", MAP_Y, MAP_X);
+	printf("\nEnd key(ESC)\n\n");
+	printf("press to start");
+	getch();
+	system("cls");
+	initGame();
+	startGame(GMap);
+	free(GMap[0]);
+	free(GMap);
+}
